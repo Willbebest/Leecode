4aeacf3bc58f0db598879e2ee651febacf5503f8
piao yang <tt19921101@163.com>
2021-07-11 21:27:10 +0800

    leetcode 104

--- Tree/leetcode0104.cc
+++ Tree/leetcode0104.cc
@@ -9,12 +9,14 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// “自底向上” 的解决方案
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(root==nullptr) return 0;
-        int l = maxDepth(root->left);
-        int r = maxDepth(root->right);
-        return max(l, r) + 1;
+        int left_height = maxDepth(root->left);
+        int right_height = maxDepth(root->right);
+        return max(left_height, right_height) + 1;
     }
 };
+
